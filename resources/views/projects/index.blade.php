7ce790c578359c85b0da49a3675654ceceee7615
Osama Hamed <osamahamedmohammed@gmail.com>
2019-05-26 01:44:24 +0200

    Style the dashboard and the single project page

--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -1,15 +1,20 @@
 @extends('layouts.app')
 
 @section('content')
-	<h1>Birdboard</h1>
+	<header class="flex items-center mb-3 py-4">
+		<div class="flex justify-between items-end w-full">
+			<h2 class="text-grey text-sm font-normal">My Projects</h2>
+			<a href="/projects/create" class="button">New Project</a>
+		</div>
+	</header>
 
-	<ul>
+	<main class="lg:flex lg:flex-wrap -mx-3">
 		@forelse($projects as $project)
-			<li>
-				<a href="{{ $project->path() }}">{{ $project->title }}</a>
-			</li>
+			<div class="lg:w-1/3 px-3 pb-6">
+				@include ('projects.card')
+			</div>
 		@empty
-			<li>No projects yet.</li>
+			<div>No projects yet.</div>
 		@endforelse
-	</ul>
+	</main>
 @endsection

90b0a03eeff26d68a1979ade660a4862f5f84905
Osama Hamed <osamahamedmohammed@gmail.com>
2019-05-29 03:51:07 +0200

    Style the registration, login and create project forms

--- resources/views/projects/create.blade.php
+++ resources/views/projects/create.blade.php
@@ -1,19 +1,38 @@
 @extends('layouts.app')
 
 @section('content')
-	<h1>Create New Project</h1>
-
-	<form action="/projects" method="POST">
+		<form method="POST" action="/projects" class="lg:w-1/2 lg:mx-auto bg-white py-12 px-16 rounded shadow">
 		@csrf
 
-		<label for="title">Title:</label>
-		<input type="text" name="title" id="title">
+		<h1 class="text-2xl font-normal mb-10 text-center">Let’s start something new</h1>
 
-		<br>
+		<div class="field mb-6">
+            <label class="label text-sm mb-2 block" for="title">Title</label>
+            <div class="control">
+                <input type="text" class="input bg-transparent border border-grey-light rounded p-2 text-xs w-full" name="title" placeholder="My next awesome project">
+            </div>
 
-		<label for="description">Description:</label>
-		<textarea name="description" id="description"></textarea>
+            @error('title')
+                <p class="text-red text-xs mt-2">{{ $message }}</p>
+            @enderror
+        </div>
 
-		<button type="submit">Send</button>
+        <div class="field mb-6">
+            <label class="label text-sm mb-2 block" for="description">Description</label>
+            <div class="control">
+                <textarea name="description" rows="10" class="textarea bg-transparent border border-grey-light rounded p-2 text-xs w-full" placeholder="I should start learning piano."></textarea>
+            </div>
+
+            @error('description')
+                <p class="text-red text-xs mt-2">{{ $message }}</p>
+            @enderror
+        </div>
+
+        <div class="field">
+        	<div class="control">
+                <button type="submit" class="button is-link mr-2">Create Project</button>
+                <a href="/projects">Cancel</a>
+        	</div>
+        </div>
 	</form>
 @endsection

0e34cb07a84a41989c2526484703492c84d344b0
Osama Hamed <osamahamedmohammed@gmail.com>
2019-07-05 10:37:14 +0200

    Allow users to delete their own projects

--- resources/views/projects/card.blade.php
+++ resources/views/projects/card.blade.php
@@ -3,5 +3,14 @@
 		<a href="{{ $project->path() }}" class="text-black no-underline">{{ $project->title }}</a>
 	</h3>
 
-	<div class="text-grey">{{ Str::limit($project->description) }}</div>
+	<div class="text-grey mb-4">{{ Str::limit($project->description) }}</div>
+
+	<footer>
+        <form method="POST" action="{{ $project->path() }}" class="text-right">
+            @method('DELETE')
+            @csrf
+            
+            <button type="submit" class="text-xs">Delete</button>
+        </form>
+    </footer>
 </div>
